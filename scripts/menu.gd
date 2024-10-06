3509a7986033a6f9929a0efa0ad42d46bef6926e
Tristan Lasalle <tristan.lasalle@gmail.com>
2024-10-06 11:02:27 +0200

    First iteration of menu, need polish

---
+++ scripts/menu.gd
@@ -0,0 +1,14 @@
+extends Control
+
+
+
+func _on_classic_mode_button_pressed() -> void:
+    self.get_tree().change_scene_to_file("res://scenes/ld_56.tscn")
+
+
+func _on_survival_mode_button_pressed() -> void:
+    pass # Replace with function body.
+
+
+func _on_creative_mode_button_pressed() -> void:
+    pass # Replace with function body.
